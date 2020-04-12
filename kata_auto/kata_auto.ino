075f55c430a32a97a9ab86ac1ec40ab6ab416f77
meguko2002 <meguko2002@gmail.com>
2020-04-12 21:20:33 +0900

    ServoDrive クラスを作ってコンパイルエラーは無かったが、動作が正常かは未検証

--- kata_auto/kata_auto.ino
+++ kata_auto/kata_auto.ino
@@ -1,42 +1,73 @@
 #include <Servo.h>
 
-const int MOTOR_DELAY_US = 320;   // 0.32msec/pulse or 2.67msec/deg
-// 0deg: 800ps,  90deg: 1550ps,  180deg: 2200ps
-#define MIN_POS  800
-#define MAX_POS  2200
+const int com[10] = { -1, 5, 6, 7, 4, 8, 0, 3, 2, 1}; //com[key]
 
-const int pos_range = (MAX_POS - MIN_POS) / 2;
-const int ctr_pos = (MAX_POS + MIN_POS) / 2;
-const int dir[9][2] = {  //dir[command]
-  {  1,  0},  //'dr',key='3'
-  {  1,  1},  //'dd',key='2'
-  {  0,  1},  //'dl',key='1'
-  { -1,  1},  //'ll', key='4'
-  { -1,  0},  //'ul',key='7'
-  { -1, -1},  //'uu',key='8'
-  {  0, -1},  //'ur',key='9'
-  {  1, -1},   //'rr',key='6'
-  { 0, 0}     //'rr',key='5'
+class ServoDrive
+{
+  public :
+    ServoDrive(int pin_servo_l, int spin_servo_r);
+    void tilt_field(int command);
+    void set_pos();
+    int command;
+    int get_pos_l();
+    int get_pos_r();
+
+  private :
+    const int dir[9][2] = {  //dir[command]
+      {  1,  0},  //'dr',key='3'
+      {  1,  1},  //'dd',key='2'
+      {  0,  1},  //'dl',key='1'
+      { -1,  1},  //'ll', key='4'
+      { -1,  0},  //'ul',key='7'
+      { -1, -1},  //'uu',key='8'
+      {  0, -1},  //'ur',key='9'
+      {  1, -1},   //'rr',key='6'
+      { 0, 0}     //'rr',key='5'
+    };
+    const int dir_num = sizeof dir / sizeof dir[0] ;
+    const int pos_range = (MAX_POS - MIN_POS) / 2;
+    const int ctr_pos = (MAX_POS + MIN_POS) / 2;
+    const int MOTOR_DELAY_US = 320;   // 0.32msec/pulse or 2.67msec/deg
+    // 0deg: 800ps,  90deg: 1550ps,  180deg: 2200ps
+    const int MIN_POS = 800;
+    const int MAX_POS = 2200;
+
+    int o_pos[2];
+    int pos[2] =  {ctr_pos, ctr_pos};
+    int target[2] , pt[2], act_index[2];
+    const int act_num = 2;
+    int next_action_list[2][2];
+    int pre_command = 0;
+    Servo servo_l, servo_r;
+
 };
 
-const int com[10] = {-1, 5, 6, 7, 4, 8, 0, 3, 2, 1};  //com[key]
-const int dir_num = sizeof dir / sizeof dir[0] ;
+ServoDrive::ServoDrive(int pin_servo_l, int pin_servo_r)
+{
+  servo_l.attach(pin_servo_l, MIN_POS, MAX_POS);
+  servo_r.attach(pin_servo_r, MIN_POS, MAX_POS);
 
+}
 
-#define SERVO_L 9
-#define SERVO_R 6
+int ServoDrive::get_pos_l()
+{
+  return pos[0];
+}
 
-Servo servo_l, servo_r;
+int ServoDrive::get_pos_r()
+{
+  return pos[1];
+}
 
-void tilt_field(int command, int *pos) {
-  static int pre_command = -1;
-  static int o_pos[2] = {ctr_pos, ctr_pos};
-  int target[2] , pt[2], act_index[2];
-  const int act_num = 2;
-  int next_action_list[2][2];
+void ServoDrive::set_pos()
+{
+  pos[0] = ctr_pos;
+  pos[1] = ctr_pos;
+}
+
+void ServoDrive::tilt_field(int command) {
   pos[0] = servo_l.readMicroseconds();
   pos[1] = servo_r.readMicroseconds();
-
   for (int i = 0; i < 2; i++) target[i] = dir[command][i] * pos_range + ctr_pos;
   for (int i = 0; i < 2; i++) pt[i] = target[i] - pos[i];
 
@@ -108,42 +139,34 @@ void tilt_field(int command, int *pos) {
     }
   }
   for (int i = 0; i < 2; i++)  pos[i] = constrain(pos[i] + dir[act][i], MIN_POS, MAX_POS);
-    servo_l.writeMicroseconds(pos[0]);
-    servo_r.writeMicroseconds(pos[1]);
+  servo_l.writeMicroseconds(pos[0]);
+  servo_r.writeMicroseconds(pos[1]);
 }
 
+
+#define SERVO_L 9
+#define SERVO_R 6
+
 void setup() {
-  pinMode(SERVO_L,  OUTPUT);
-  pinMode(SERVO_R,  OUTPUT);
-  servo_l.attach(SERVO_L, MIN_POS, MAX_POS);
-  servo_r.attach(SERVO_R, MIN_POS, MAX_POS);
   Serial.begin(9600);
 }
 
 void loop() {
   char key;
   int command;
-  static int pos[2] = {ctr_pos, ctr_pos};
   unsigned long time;
+  int pos[2];
+  ServoDrive sv = ServoDrive(SERVO_L, SERVO_R);   //(9pin, 6pin)
   while (1) {
     if ( Serial.available() > 0 ) {
       key = Serial.read(); //1~9で8方向を表現
       command = com[key]; //十字キーを使う際は
-      if (command == -1) {
-        Serial.println("input 1-9");
-      }
-      else {
-        time = millis();
-        for (int i = 0; i < 1000; i++) {
-          tilt_field(command, pos);
-//          Serial.print(pos[0]);
-//          Serial.print(",");
-//          Serial.println(pos[1]);
-        }
-      }
-      Serial.print("1000times :");
-      Serial.println(millis() - time);
-      break;
+
+      sv.tilt_field(command);
+
+      Serial.print(sv.get_pos_l());
+      Serial.print(",");
+      Serial.println(sv.get_pos_r());
     }
   }
 
