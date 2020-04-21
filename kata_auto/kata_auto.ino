11b48af9daf5f38ca00af9a16c9d0b0d2782340d
meguko <meguko2002@gmail.com>
2020-04-21 13:01:04 +0900

    余計なファイルを削除

--- kata_auto/kata_auto.ino
+++ kata_auto/kata_auto.ino
@@ -22,7 +22,6 @@ const int dir[9][2] = {  //dir[command]
 const int keyset[9] = {6, 9, 8, 7, 4, 1, 2, 3, 5};
 const int dir_num = sizeof dir / sizeof dir[0] ;
 
-
 #define SERVO_L 9
 #define SERVO_R 6
 
@@ -32,14 +31,14 @@ int get_command_from_key(int key) {
   Serial.println(key);
   int command = -1;
   for (int i = 0; i < dir_num ; i++) {
-    if (key == keyset[i]) command = i;
+	if (key == keyset[i]) command = i;
   }
   Serial.println(command);
   return command;
 }
 
 void tilt_field(int command, int *pos) {
-  static int pre_command = -1;
+   static int pre_command = -1;
   static int o_pos[2] = {ctr_pos, ctr_pos};
   int target[2] , pt[2], act_index[2];
   const int act_num = 2;
@@ -52,74 +51,74 @@ void tilt_field(int command, int *pos) {
 
   if (pt[0] == 0 & pt[1] == 0) return; //なにもしない
   else if (pt[0] > 0) {
-    if (pt[1] > 0) {
-      if (pt[1] < pt[0]) {
-        act_index[0] = 0;
-        act_index[1] = 1;
-      }
-      else {
-        act_index[0] = 1;
-        act_index[1] = 2;
-      }
-    }
-    else {
-      if (pt[1] < -pt[0]) {
-        act_index[0] = 6;
-        act_index[1] = 7;
-      }
-      else {
-        act_index[0] = 7;
-        act_index[1] = 0;
-      }
-    }
+	if (pt[1] > 0) {
+	  if (pt[1] < pt[0]) {
+		act_index[0] = 0;
+		act_index[1] = 1;
+	  }
+	  else {
+		act_index[0] = 1;
+		act_index[1] = 2;
+	  }
+	}
+	else {
+	  if (pt[1] < -pt[0]) {
+		act_index[0] = 6;
+		act_index[1] = 7;
+	  }
+	  else {
+		act_index[0] = 7;
+		act_index[1] = 0;
+	  }
+	}
   }
   else {
-    if (pt[1] > 0) {
-      if (pt[1] > -pt[0]) {
-        act_index[0] = 2;
-        act_index[1] = 3;
-      }
-      else {
-        act_index[0] = 3;
-        act_index[1] = 4;
-      }
-    }
-    else {
-      if (pt[1] > pt[0]) {
-        act_index[0] = 4;
-        act_index[1] = 5;
-      }
-      else {
-        act_index[0] = 5;
-        act_index[1] = 6;
-      }
-    }
+	if (pt[1] > 0) {
+	  if (pt[1] > -pt[0]) {
+		act_index[0] = 2;
+		act_index[1] = 3;
+	  }
+	  else {
+		act_index[0] = 3;
+		act_index[1] = 4;
+	  }
+	}
+	else {
+	  if (pt[1] > pt[0]) {
+		act_index[0] = 4;
+		act_index[1] = 5;
+	  }
+	  else {
+		act_index[0] = 5;
+		act_index[1] = 6;
+	  }
+	}
   }
   int act, on[2], ot[2], inner_onot;
   float cos2, cosmax, norm_on, norm_ot;
 
   if (command == pre_command) for (int i = 0; i < 2; i++) {
-      ot[i] = target[i] - o_pos[i];
-    }
+	  ot[i] = target[i] - o_pos[i];
+	}
   else for (int i = 0; i < 2; i++) o_pos[i] = pos[i];
   pre_command = command;
 
   for (int i = 0; i < 2; i++) {
-    for (int j = 0 ; j < 2; j++) {
-      on[j] = pos[j] + dir[act_index[i]][j] - o_pos[j];
-    }
-    inner_onot = on[0] * ot[0] + on[1] * ot[1];
-    norm_on = sqrt(on[0] * on[0] + on[1] * on[1]);
-    norm_ot = sqrt(ot[0] * ot[0] + ot[1] * ot[1]);
-    cos2 = inner_onot / norm_on / norm_ot;
-    if (i == 0 | cosmax < cos2) {
-      cosmax = cos2;
-      act = act_index[i];
-    }
+	for (int j = 0 ; j < 2; j++) {
+	  on[j] = pos[j] + dir[act_index[i]][j] - o_pos[j];
+	}
+	inner_onot = on[0] * ot[0] + on[1] * ot[1];
+	norm_on = sqrt(on[0] * on[0] + on[1] * on[1]);
+	norm_ot = sqrt(ot[0] * ot[0] + ot[1] * ot[1]);
+	cos2 = inner_onot / norm_on / norm_ot;
+	if (i == 0 | cosmax < cos2) {
+	  cosmax = cos2;
+	  act = act_index[i];
+	}
   }
   for (int i = 0; i < 2; i++)  pos[i] = constrain(pos[i] + dir[act][i], MIN_POS, MAX_POS);
-    servo_l.writeMicroseconds(pos[0]);
-    servo_r.writeMicroseconds(pos[1]);
+	servo_l.writeMicroseconds(pos[0]);
+	servo_r.writeMicroseconds(pos[1]);
 }
 
 void setup() {
@@ -140,30 +139,30 @@ void loop() {
   unsigned long time;
   Serial.println("key waiting");
   while (1) {
-    if ( Serial.available() > 0 ) {
-      key = Serial.read(); //1~9で8方向を表現
-      key -= 48;
-      Serial.print("key: ");
-      Serial.println(key);
-      command = get_command_from_key(key);
-      Serial.print("command: ");
-      Serial.println(command);
-      if (command == -1) {
-        Serial.println("input 1-9");
-      }
-      else {
-        time = millis();
-        for (int i = 0; i < 1000; i++) {
-          tilt_field(command, pos);
+	if ( Serial.available() > 0 ) {
+	  key = Serial.read(); //1~9で8方向を表現
+	  key -= 48;
+	  Serial.print("key: ");
+	  Serial.println(key);
+	  command = get_command_from_key(key);
+	  Serial.print("command: ");
+	  Serial.println(command);
+	  if (command == -1) {
+		Serial.println("input 1-9");
+	  }
+	  else {
+		time = millis();
+		for (int i = 0; i < 1000; i++) {
+		  tilt_field(command, pos);
 //          Serial.print(pos[0]);
 //          Serial.print(",");
 //          Serial.println(pos[1]);
-        }
-      }
-      Serial.print("1000times :");
-      Serial.println(millis() - time);
-      break;
-    }
+		}
+	  }
+	  Serial.print("1000times :");
+	  Serial.println(millis() - time);
+	  break;
+	}
   }
 
 }
